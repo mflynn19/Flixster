d173d4328b37f62ead5ed29906972f18a15f51c9
mflynn19 <meghan.flynn@theflynn.org>
2019-10-18 10:03:22 -0400

    Initial commit

---
+++ app/src/main/java/com/androidstudioprojects/flixster/models/Movie.java
@@ -0,0 +1,42 @@
+package com.androidstudioprojects.flixster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Movie {
+    String posterPath;
+    String title;
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+        //should be fetching all of the available sizes and appending them to base URL then adding the relative path
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    String overview;
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        posterPath = jsonObject.getString("poster_path");
+        title = jsonObject.getString("title");
+        overview = jsonObject.getString("overview");
+    }
+
+    public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+        for (int i = 0; i < movieJsonArray.length(); i++){
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+        return movies;
+    }
+}
